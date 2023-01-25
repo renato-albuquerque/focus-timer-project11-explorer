7f499be381c0a27453b934916bce6c512d06171f
renato-albuquerque <renatomalb83@gmail.com>
2023-01-25 00:14:47 -0300

    updated project

---
+++ index.js
@@ -0,0 +1,122 @@
+const buttonPlay = document.querySelector('.play')
+const buttonPause = document.querySelector('.pause')
+const buttonStop = document.querySelector('.stop')
+const buttonSet = document.querySelector('.set')
+const buttonSoundOn = document.querySelector('.sound-on')
+const buttonSoundOff = document.querySelector('.sound-off')
+const minutesDisplay = document.querySelector('.minutes')
+const secondsDisplay = document.querySelector('.seconds')
+let minutes = Number(minutesDisplay.textContent)
+let timerTimeOut
+
+const buttonPressAudio = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/button-press.wav?raw=true")
+const kitchenTimer = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/kichen-timer.mp3?raw=true")
+const bgAudio = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/bg-audio.mp3?raw=true")
+
+function resetControls() {
+  buttonPlay.classList.remove('hide')
+  buttonPause.classList.add('hide')
+  buttonSet.classList.remove('hide')
+  buttonStop.classList.add('hide')
+}
+
+function updateTimerDisplay(minutes, seconds) {
+  minutesDisplay.textContent = String(minutes).padStart(2, "0")
+  secondsDisplay.textContent = String(seconds).padStart(2, "0")
+}
+
+function resetTimer() {
+  updateTimerDisplay(minutes, 0)
+  clearTimeout(timerTimeOut)
+}
+
+function pressButton() {
+  buttonPressAudio.play()
+}
+
+function timeEnd() {
+  kitchenTimer.play()
+}
+
+function backGroundSoundPlay() {
+  bgAudio.play()
+}
+
+function backGroundSoundPause() {
+  bgAudio.pause()
+}
+
+function countdown(){
+  timerTimeOut = setTimeout(function() {
+    let seconds =  Number(secondsDisplay.textContent)
+    let minutes = Number(minutesDisplay.textContent)
+    let isFinished = minutes <= 0 && seconds <= 0 
+
+    updateTimerDisplay(minutes, 0)
+
+    if (isFinished) {
+      resetControls()
+      updateTimerDisplay()
+      timeEnd()
+      return
+    }
+
+    if( seconds <= 0 ) {
+      seconds = 6
+      --minutes
+    }
+
+    updateTimerDisplay(minutes, String(seconds - 1))
+
+    countdown()
+  }, 1000)
+}
+
+buttonPlay.addEventListener('click', function() {
+  buttonPlay.classList.add('hide')
+  buttonPause.classList.remove('hide')
+  buttonSet.classList.add('hide')
+  buttonStop.classList.remove('hide')
+
+  countdown()
+  pressButton()
+})
+
+buttonPause.addEventListener('click', function() {
+  buttonPause.classList.add('hide')
+  buttonPlay.classList.remove('hide')
+  clearTimeout(timerTimeOut)
+  pressButton()
+})
+
+buttonStop.addEventListener('click', function() {
+  resetControls()
+  resetTimer()
+  pressButton()
+})
+
+buttonSet.addEventListener('click', function() {
+  let newMinutes = prompt('How many minutes?')
+  if (!newMinutes) {
+    resetTimer()
+    return
+  }
+
+  minutes = newMinutes
+  updateTimerDisplay(minutes, 0)
+})
+
+buttonSoundOff.addEventListener('click', function() {
+  buttonSoundOn.classList.remove('hide')
+  buttonSoundOff.classList.add('hide')
+
+  backGroundSoundPlay()
+})
+
+buttonSoundOn.addEventListener('click', function() {
+  buttonSoundOn.classList.add('hide')
+  buttonSoundOff.classList.remove('hide')
+
+  backGroundSoundPause()
+})
+
